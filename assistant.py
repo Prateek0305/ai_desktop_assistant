dcc9200a5f5f98f962b637198dc1d0e53c438503
Prateek Agrawal <138208959+Prateek0305@users.noreply.github.com>
2025-02-25 15:46:52 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,1 +1,95 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import warnings
+import webbrowser
+import os
+import smtplib
 
+
+engine = pyttsx3.init('sapi5')
+voices= engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):
+    engine.say(audio) 
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good afternoon")
+
+    speak("hello! I am Erica. Please tell me how can i help you")
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing..")
+        query = r.recognize_google(audio, language= 'en-in')
+        print(f"User said:{query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+warnings.filterwarnings("ignore", category=UserWarning, module='wikipedia')
+if __name__=="__main__" :
+    wishMe()
+    while True:
+        query= takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences =2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'campus' in query:
+            webbrowser.open("youtube.com/watch?v=ZftI2fEz0Fw&list=PLKnIA16_Rmvbr7zKYQuBfsVkjoLcJgxHH")
+        elif 'play music' in query:
+            music_dir= 'D:\\the world of 90s'
+            songs = os.listdir(music_dir)
+            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Prateek Agrawal\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code"
+            os.startfile(codePath)
+        
+        def sendEmail(to, content):
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.login('....email id....', '....password....')
+            server.sendmail('....email id....', to, content)
+            server.close()
+
+            elif 'email to Prateek' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "prateek03050@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry. I am not able to send this email")    
